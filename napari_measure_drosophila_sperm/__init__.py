81fef472580e01e6c44467e4191a5bdeb443a35b
ypark219 <yeonsoopark@wustl.edu>
2024-11-07 15:35:51 -0600

    new thresholding

--- napari_measure_drosophila_sperm/__init__.py
+++ napari_measure_drosophila_sperm/__init__.py
@@ -3,46 +3,49 @@ import napari
 from magicgui import magic_factory
 
 import numpy as np
+import skimage
 from skimage import data
-from skimage.filters import threshold_otsu
 from skimage.segmentation import clear_border
 from skimage.measure import label
 from skimage.morphology import closing, square, remove_small_objects
 from skimage.color import rgb2gray
 
-@magic_factory
-#my edited version. it works on images now but doesn't convert to greyscale
-def threshold(
-    image: "napari.layers.Image",
-    threshold: int = 200,
-) -> "napari.types.LayerDataTuple":
-    #show_info("image rgb: {x}".format(x=image.rgb))
-    data = rgb2gray(image.data > threshold) #`rgb2gray(image.data) > threshold` doesn't work for some reason ㅠㅠ
-    metadata = {"name": "thresholded"} # colormap doesn't seem to take effect. setting "rgb": False breaks things
-    layer_type = "image" # i want to return a Labels layer but setting layer_type to "labels" and changing data to (image > threshold).astype(int) doesn't give a proper output
-    return (data, metadata, layer_type)
+# @magic_factory
+# #my edited version. it works on images now but doesn't convert to greyscale
+# def threshold(
+#     image: "napari.layers.Image",
+#     threshold: int = 200,
+# ) -> "napari.types.LayerDataTuple":
+#     #show_info("image rgb: {x}".format(x=image.rgb))
+#     data = rgb2gray(image.data > threshold) #`rgb2gray(image.data) > threshold` doesn't work for some reason ㅠㅠ
+#     metadata = {"name": "thresholded"} # colormap doesn't seem to take effect. setting "rgb": False breaks things
+#     layer_type = "image" # i want to return a Labels layer but setting layer_type to "labels" and changing data to (image > threshold).astype(int) doesn't give a proper output
+#     return (data, metadata, layer_type)
 
 
 
 
 # trying with otsu thresholding
-# @magic_factory
-# # from https://napari.org/0.5.2/tutorials/segmentation/annotate_segmentation.html#segmentation
-# def threshold(
-#     image: "napari.types.ImageData"
-# ) -> "napari.types.LayerDataTuple":
-#     grey = rgb2gray(image)
-#     # apply threshold
-#     thresh = threshold_otsu(grey)
-#     bw = closing(grey > thresh, square(4))
+@magic_factory
+# from https://napari.org/0.5.2/tutorials/segmentation/annotate_segmentation.html#segmentation
+def threshold(
+    image: "napari.types.ImageData"
+    # ,
+    # min_size: int=1,
+    # conn: int=1
+) -> "napari.types.LayerDataTuple":
+    grey = rgb2gray(image)
+    # apply threshold
+    # options: isodata, li, mean, minimum, otsu, triangle, yen, niblack, sauvola
 
-#     # remove artifacts connected to image border
-#     cleared = remove_small_objects(clear_border(bw), 20)
+    thresh = skimage.filters.threshold_niblack(grey)
+    # bw = closing(grey > thresh, square(4))
+    bw=grey>thresh
 
-#     # label image regions
-#     label_image = label(cleared)
+    # remove artifacts connected to image border
+    # cleared = remove_small_objects(clear_border(bw), min_size,conn)
 
-#     return (label_image, {"name": "segmentation"}, "labels")
+    return (bw, {"name": "segmentation"}, "labels")
 
 
 

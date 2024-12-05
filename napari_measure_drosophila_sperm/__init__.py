668055e7caaea6b647fd6cebe58190784d732fa3
ypark219 <yeonsoopark@wustl.edu>
2024-12-04 20:39:04 -0600

    add downscaling to driver2

--- napari_measure_drosophila_sperm/__init__.py
+++ napari_measure_drosophila_sperm/__init__.py
@@ -5,17 +5,10 @@ import skimage
 from . import threshold, util, get_selection, blur, measure
 
 # plugin order of operations:
-# - "driver"
-# - remove small objs
-# - skeletonise
-# - (opt) get endpoints and re-skeletonise
-# - measure
-
-# TODO:
-# - post-threshing modifications currently don't remove enough noise so try to break up some of the bigger chunks so they are all removed with remove_small_objs
-# - large glare in WT.C.2_20x (medium)
-# - poor results on cells with overlapping loops (hard cases)
-# otsu min_size? survey all components and try to find a "threshold" for size
+#  1. driver2
+#  2. remove small objs (5 or 10)
+#  3. get endpoints (8) and reskeleton
+#  4. get largest component and measure
 
 
 # blurs, thresholds, and skeletonises a selection of a downscaled (50%) image
@@ -49,15 +42,31 @@ def driver(
     result = opened
     return (result, {"name": "result"}, "image")
 
+
 @magic_factory
 def driver2(
     image: "napari.layers.Image",
     shape: "napari.layers.Shapes"
 ) -> "napari.types.LayerDataTuple":
-    selection = get_selection.get_selection(image.data, shape)
+    SCALE = 0.5
+    viewer = napari.current_viewer()
+    
+    # rescale image and selection (if Shape exists), and remove originals
+    downscaled = skimage.transform.rescale(util.greyize(image.data), SCALE, anti_aliasing=True)
+    viewer.layers.remove(image.name)
+    threshed = threshold.thresh2(downscaled, 2, 2)
 
-    threshed = threshold.thresh2(selection, 2, 2)
-    skel = skimage.morphology.skeletonize(threshed.astype(bool), method="zhang")
+    # if no shape is given, apply to whole image
+    if shape is not None:
+        shape_small = shape
+        shape_small.scale = shape.scale * SCALE
+        shape_small.data = np.multiply(shape.data, SCALE)
+        viewer.layers.remove(shape.name)
+        selection = get_selection.get_selection(threshed, shape_small)
+    else:
+        selection = threshed\
+
+    skel = skimage.morphology.skeletonize(selection.astype(bool), method="zhang")
 
     result = skel
     return (result, {"name": "result"}, "image")
